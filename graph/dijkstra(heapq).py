8a9d392bb129dcd891b1cb8611d54d6d612d40a6
piu0887 <147304229+piu0887@users.noreply.github.com>
2023-10-27 00:25:04 +0900

    2week final commit

--- graph/dijkstra(heapq).py
+++ graph/dijkstra(heapq).py
@@ -1,35 +1,40 @@
 import sys
 import heapq
+
 input = sys.stdin.readline
-n, m = map(int, input().split())
-start = int(input())
-INF = int(1e9)
-distance = [INF] * (n+1)
-graph = [[] for _ in range(n+1)]
-for _ in range(m) :
-    a, b, c = map(int, input().split())
-    graph[a].append((b,c))
 
-def dijkstra (start) :
-    que = []
-    heapq.heappush(que, (0, start)) # 시작 노드 정보 우선순위 큐에 삽입
-    distance[start] = 0
-    while que:
-        dist, node = heapq.heappop(que)
-        #큐에서 뽑아낸 거리가 이미 갱신된 거리보다 클 경우 (=방문한 셈) 무시
-        if distance[node] < dist:
+N= int(input())
+M= int(input())
+
+
+
+graph = [[] for _ in range(N+1)]
+for _ in range(M):
+    a,b,c = map(int,input().split())
+    graph[a].append((b,c))       #도착지 가중치 정보 삽입
+
+start, end = map(int, input().split())
+
+def dijkstra(graph,start):
+    queue = []
+    INF = int(1e9) 
+    distances = [INF] * (N+1)    # 처음값 초기화
+    distances[start] = 0         #시작 노드까지의 거리 0
+    heapq.heappush(queue,[distances[start],start])   # 시작노드부터 탐색
+    
+    while queue:                         # queue에 노드가 없을때 까지
+        dist,node = heapq.heappop(queue) # 탐색할 노드, 거리
+        # 기존 최단거리보다 멀다면 무시
+        if distances[node] < dist :
             continue
-        #큐에서 뽑아낸 노드와 연결된 인접노드들 탐색
-        for next in graph[node]:
-            cost = distance[node] + next[1] # 시작->노드거리 + node ->node 의 인접 노드 거리
-            if cost < distance[next[0]] :   # cost < 시작 -> node의 인접노드 거리
-                    distance[next[0]] = cost
-                    heapq.heappush(que,(cost, next[0])) # 우선순위큐에 업데이트
-
-dijkstra(start)
-
-for i in range(1, len(distance)) :
-    if distance[i] == INF:
-          print('도달할수 없음')
-    else:
-         print(distance[i])
+        # 노드와 연결된 인접노드 탐색
+        for next_node, next_dist in graph[node]:
+            distance = dist + next_dist # 인접노드까지의 거리
+            if distance < distances[next_node]: # 기존 거리보다 짧으면 갱신
+                distances[next_node] = distance
+                # 다음 인접 거리를 계산하기위에 queue에 삽입
+                heapq.heappush(queue,[distance, next_node])
+    return distances
+            
+dist_start = dijkstra(graph,start)
+print(dist_start[end])
